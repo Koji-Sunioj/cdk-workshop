809a7dd9db6a5dd813447cd7eb07eeb6f44223a6
Koji-Sunioj <koji.gabriel218@gmail.com>
2023-01-02 23:45:30 +0200

    with dynamo

---
+++ lambda/hello.js
@@ -0,0 +1,93 @@
+const AWS = require("aws-sdk");
+const docClient = new AWS.DynamoDB.DocumentClient({ region: "eu-north-1" });
+
+exports.handler = async function (event, context) {
+  const { httpMethod, resource, pathParameters, body } = event;
+  const routeKey = `${httpMethod} ${resource}`;
+
+  let returnObject = {};
+
+  let params = {
+    TableName: process.env.ALBUM_TABLE_NAME,
+  };
+
+  switch (routeKey) {
+    case "GET /albums":
+      const { Items: albums } = await docClient.scan(params).promise();
+      returnObject = { albums: albums };
+      break;
+    case "POST /albums":
+      ({ title, released, artist } = JSON.parse(body));
+      const toBeCreated = {
+        title,
+        released,
+        artist,
+        albumId: context.awsRequestId,
+      };
+
+      await docClient
+        .put({
+          ...params,
+          Item: toBeCreated,
+        })
+        .promise();
+      returnObject = {
+        album: toBeCreated,
+        message: "album successfully created",
+      };
+
+      break;
+    case "DELETE /albums/{albumId}":
+      await docClient
+        .delete({
+          ...params,
+          Key: { albumId: pathParameters.albumId },
+        })
+        .promise();
+      returnObject = {
+        message: `album ${pathParameters.albumId} successfully deleted`,
+      };
+      break;
+    case "PATCH /albums/{albumId}":
+      ({ title, released, artist } = JSON.parse(body));
+      const toBeUpdated = {
+        title,
+        released,
+        artist,
+        albumId: pathParameters.albumId,
+      };
+
+      await docClient
+        .update({
+          ...params,
+          Key: { albumId: pathParameters.albumId },
+          UpdateExpression:
+            "SET title = :title, artist = :artist, released = :released",
+          ExpressionAttributeValues: {
+            ":title": title,
+            ":released": Number(released),
+            ":artist": artist,
+          },
+        })
+        .promise();
+      returnObject = {
+        album: toBeUpdated,
+        message: `album ${pathParameters.albumId} successfully updated`,
+      };
+      break;
+    case "GET /albums/{albumId}":
+      const { Item: album } = await docClient
+        .get({
+          ...params,
+          Key: { albumId: pathParameters.albumId },
+        })
+        .promise();
+      returnObject = { album: album };
+      break;
+  }
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(returnObject),
+  };
+};
--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,24 +1,42 @@
 import * as cdk from "aws-cdk-lib";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as apigw from "aws-cdk-lib/aws-apigateway";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 import { HitCounter } from "./hitcounter";
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const table = new dynamodb.Table(this, "Albums", {
+      partitionKey: { name: "albumId", type: dynamodb.AttributeType.STRING },
+    });
+
     const hello = new lambda.Function(this, "HelloHandler", {
-      runtime: lambda.Runtime.NODEJS_14_X, // execution environment
-      code: lambda.Code.fromAsset("lambda"), // code loaded from "lambda" directory
-      handler: "hello.handler", // file is "hello", function is "handler"
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset("lambda"),
+      handler: "hello.handler",
+      environment: {
+        ALBUM_TABLE_NAME: table.tableName,
+      },
     });
+    table.grantReadWriteData(hello);
+    // const helloWithCounter = new HitCounter(this, "HelloHitCounter", {
+    //   downstream: hello,
+    // });
 
-    const helloWithCounter = new HitCounter(this, "HelloHitCounter", {
-      downstream: hello,
+    const api = new apigw.LambdaRestApi(this, "Endpoint", {
+      handler: hello,
+      // handler: helloWithCounter.handler,
+      proxy: false,
     });
+    const albums = api.root.addResource("albums");
+    albums.addMethod("GET");
+    albums.addMethod("POST");
 
-    new apigw.LambdaRestApi(this, "Endpoint", {
-      handler: helloWithCounter.handler,
-    });
+    const album = albums.addResource("{albumId}");
+    album.addMethod("GET");
+    album.addMethod("DELETE");
+    album.addMethod("PATCH");
   }
 }
