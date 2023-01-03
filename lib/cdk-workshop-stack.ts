4af223b63ff0dc807826c286340a11db082dc94d
Koji-Sunioj <koji.gabriel218@gmail.com>
2023-01-03 16:50:49 +0200

    static website

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -2,16 +2,48 @@ import * as cdk from "aws-cdk-lib";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as apigw from "aws-cdk-lib/aws-apigateway";
 import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
-import { HitCounter } from "./hitcounter";
+//import { HitCounter } from "./hitcounter";
+
+//s3 and cloud front deployment imports
+import { Bucket, BucketAccessControl } from "aws-cdk-lib/aws-s3";
+import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
+import * as path from "path";
+import { Distribution, OriginAccessIdentity } from "aws-cdk-lib/aws-cloudfront";
+import { S3Origin } from "aws-cdk-lib/aws-cloudfront-origins";
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    //s3 and cloud front deployment constructors
+    const bucket = new Bucket(this, "Bucket", {
+      accessControl: BucketAccessControl.PRIVATE,
+    });
+
+    new BucketDeployment(this, "BucketDeployment", {
+      destinationBucket: bucket,
+      sources: [Source.asset(path.resolve(__dirname, "../website"))],
+    });
+
+    const originAccessIdentity = new OriginAccessIdentity(
+      this,
+      "OriginAccessIdentity"
+    );
+    bucket.grantRead(originAccessIdentity);
+
+    new Distribution(this, "Distribution", {
+      defaultRootObject: "index.html",
+      defaultBehavior: {
+        origin: new S3Origin(bucket, { originAccessIdentity }),
+      },
+    });
+
+    //table constructor
     const table = new dynamodb.Table(this, "Albums", {
       partitionKey: { name: "albumId", type: dynamodb.AttributeType.STRING },
     });
 
+    //lambda constructor
     const hello = new lambda.Function(this, "HelloHandler", {
       runtime: lambda.Runtime.NODEJS_14_X,
       code: lambda.Code.fromAsset("lambda"),
@@ -20,16 +52,17 @@ export class CdkWorkshopStack extends cdk.Stack {
         ALBUM_TABLE_NAME: table.tableName,
       },
     });
-    table.grantReadWriteData(hello);
-    // const helloWithCounter = new HitCounter(this, "HelloHitCounter", {
-    //   downstream: hello,
-    // });
 
+    //permission for lambda to invoke table ops
+    table.grantReadWriteData(hello);
+
+    //api gateway constructor
     const api = new apigw.LambdaRestApi(this, "Endpoint", {
       handler: hello,
-      // handler: helloWithCounter.handler,
       proxy: false,
     });
+
+    //creating paths and methods for api
     const albums = api.root.addResource("albums");
     albums.addMethod("GET");
     albums.addMethod("POST");
