f00a46517914838ae26329c9c5044fd812b5068c
Koji-Sunioj <koji.gabriel218@gmail.com>
2023-01-14 23:14:44 +0200

    carousel

--- website/tests3/src/pages/CreateAlbum.js
+++ website/tests3/src/pages/CreateAlbum.js
@@ -1,36 +1,33 @@
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
-import Card from "react-bootstrap/Card";
+import Carousel from "react-bootstrap/Carousel";
 import Button from "react-bootstrap/esm/Button";
 import Container from "react-bootstrap/Container";
 import InputGroup from "react-bootstrap/InputGroup";
 import CloseButton from "react-bootstrap/CloseButton";
 
-import { getSignedUrl } from "../utils/albumApi";
 import NotFound from "./NotFound";
+import { getSignedUrl } from "../utils/albumApi";
 
 import { globalContext } from "../App";
 import { useState, useContext } from "react";
 
 const CreateAlbum = () => {
+  const [index, setIndex] = useState(0);
   const [login] = useContext(globalContext);
   const [previews, setPreviews] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  const test = async (event) => {
+  const createAlbum = async (event) => {
     setLoading(true);
     event.preventDefault();
     const { AccessToken } = login;
-
-    const {
-      title: { value: title },
-    } = event.currentTarget;
-
+    let dynamoAlbum;
     const dynamoData = [];
     const responses = await Promise.all(
       previews.map(async (item) => {
-        const { name, type, file, text } = item;
+        const { name, type, file, text, order } = item;
         const { url } = await getSignedUrl({
           name: name,
           type: type,
@@ -40,23 +37,27 @@ const CreateAlbum = () => {
           method: "PUT",
           body: file,
         });
-        dynamoData.push({ url: response.url.split("?")[0], text: text });
+        dynamoData.push({
+          url: response.url.split("?")[0],
+          text: text,
+          order: order,
+        });
         mutateCopy(response.ok, file, "completed");
         return response.ok;
       })
     );
     if (responses.every((response) => response)) {
-      const dynamoAlbum = {
-        title: title,
+      dynamoAlbum = {
         photos: dynamoData,
       };
     }
     setLoading(false);
+    console.log(dynamoAlbum);
   };
 
   const previewMapping = (files) => {
     const temp = [];
-    Array.from(files).forEach((file) => {
+    Array.from(files).forEach((file, i) => {
       temp.push({
         completed: false,
         name: file.name,
@@ -65,6 +66,7 @@ const CreateAlbum = () => {
         blob: URL.createObjectURL(file),
         closed: true,
         text: null,
+        order: i + 1,
       });
     });
     return temp;
@@ -73,7 +75,6 @@ const CreateAlbum = () => {
   const albumAble = previews.length > 0 && login !== null;
 
   const mutateCopy = (newValue, file, attribute) => {
-    console.log(previews);
     const copy = [...previews];
     const index = copy.findIndex((item) => item.name === file.name);
     copy[index][attribute] = newValue;
@@ -82,6 +83,17 @@ const CreateAlbum = () => {
     }
     setPreviews(copy);
   };
+
+  const reOrder = (order) => {
+    const found = previews.find((item) => item.order === order);
+    const filtered = previews.filter((item) => item.order !== order);
+    filtered.splice(0, 0, found);
+    filtered.forEach((item, n) => (item.order = n + 1));
+    setPreviews(filtered);
+    setIndex(0);
+  };
+
+  previews.sort((a, b) => (a.order > b.order ? 1 : b.order > a.order ? -1 : 0));
 
   return (
     <>
@@ -92,14 +104,8 @@ const CreateAlbum = () => {
           <Row>
             <Col lg="5">
               <h2>Create album</h2>
-              <Form onSubmit={test} encType="multipart/form-data">
+              <Form onSubmit={createAlbum} encType="multipart/form-data">
                 <fieldset disabled={loading}>
-                  {albumAble && (
-                    <Form.Group className="mb-3">
-                      <Form.Label>Multiple files input example</Form.Label>
-                      <Form.Control type="text" name="title" />
-                    </Form.Group>
-                  )}
                   <Form.Group className="mb-3">
                     <Form.Label>Upload your files (max 10)</Form.Label>
                     <Form.Control
@@ -128,65 +134,94 @@ const CreateAlbum = () => {
               </Form>
             </Col>
           </Row>
-          {Array.from(previews).map((file) => (
-            <Row className="mt-5" key={file.name}>
-              <Col lg="5">
-                <Card>
-                  <fieldset disabled={loading}>
-                    {file.completed ? (
-                      <div style={{ color: "green" }}>&#x2705;</div>
-                    ) : (
-                      <CloseButton
-                        size="lg"
+
+          {albumAble && (
+            <Carousel
+              slide={false}
+              variant="dark"
+              style={{ backgroundColor: "lightgrey" }}
+              activeIndex={index}
+              interval={null}
+              onSelect={(i) => {
+                setIndex(i);
+              }}
+            >
+              {previews.map((file, n) => (
+                <Carousel.Item key={file.order}>
+                  {file.completed ? (
+                    <div style={{ color: "green" }}>&#x2705;</div>
+                  ) : (
+                    <CloseButton
+                      size="lg"
+                      onClick={() => {
+                        const copy = [...previews];
+                        const filtered = copy.filter(
+                          (item) => item.name !== file.name
+                        );
+                        filtered.forEach((item, n) => (item.order = n + 1));
+                        setPreviews(filtered);
+                        const dt = new DataTransfer();
+                        Array.from(previews).forEach((item) => {
+                          if (item.name !== file.name) {
+                            dt.items.add(item.file);
+                          }
+                        });
+                        setIndex(0);
+                        const input = document.getElementById("fileInput");
+                        input.files = dt.files;
+                      }}
+                    />
+                  )}
+                  <img
+                    src={file.blob}
+                    style={{
+                      width: "auto",
+                      height: "50vw",
+                      marginLeft: "auto",
+                      marginRight: "auto",
+                      objectFit: "cover",
+                      display: "block",
+                    }}
+                  />
+                  {file.order}
+                  <Carousel.Caption>
+                    <InputGroup className="mb-3" size="lg">
+                      <Button
+                        style={{
+                          backgroundColor: "#e9ecef",
+                          color: "black",
+                          border: "1px solid #ced4da",
+                        }}
+                        disabled={file.order === 1}
                         onClick={() => {
-                          const copy = [...previews];
-                          const filtered = copy.filter(
-                            (item) => item.name !== file.name
-                          );
-                          setPreviews(filtered);
-                          const dt = new DataTransfer();
-                          Array.from(previews).forEach((item) => {
-                            if (item.name !== file.name) {
-                              dt.items.add(item.file);
-                            }
-                          });
-                          const input = document.getElementById("fileInput");
-                          input.files = dt.files;
+                          reOrder(file.order);
+                        }}
+                      >
+                        {file.order === 1 ? "cover" : "push front"}
+                      </Button>
+                      <InputGroup.Checkbox
+                        onChange={(e) => {
+                          const { checked } = e.currentTarget;
+                          mutateCopy(!checked, file, "closed");
                         }}
                       />
-                    )}
-                    <Card.Img variant="top" src={file.blob} className="p-3" />
-                    <Card.Body>
-                      <InputGroup className="mb-3">
-                        <InputGroup.Checkbox
-                          aria-label="Checkbox for following text input"
-                          onChange={(e) => {
-                            const { checked } = e.currentTarget;
-                            mutateCopy(!checked, file, "closed");
-                          }}
-                        />
-                        <Form.Control
-                          type="text"
-                          placeholder="A fancy title"
-                          name="title"
-                          autoComplete="on"
-                          value={file.text === null ? "" : file.text}
-                          disabled={file.closed}
-                          onChange={(e) => {
-                            const { value } = e.currentTarget;
-                            mutateCopy(value, file, "text");
-                          }}
-                        />
-                      </InputGroup>
-                    </Card.Body>
-                  </fieldset>
-                  <Card.Footer>
-                    <small className="text-muted">{file.name}</small>
-                  </Card.Footer>
-                </Card>
-              </Col>
-            </Row>
-          ))}
+                      <Form.Control
+                        type="text"
+                        placeholder="A fancy title"
+                        name="title"
+                        value={file.text === null ? "" : file.text}
+                        disabled={file.closed}
+                        onChange={(e) => {
+                          const { value } = e.currentTarget;
+                          mutateCopy(value, file, "text");
+                        }}
+                      />
+                    </InputGroup>
+                  </Carousel.Caption>
+                </Carousel.Item>
+              ))}
+            </Carousel>
+          )}
         </Container>
       )}
     </>
